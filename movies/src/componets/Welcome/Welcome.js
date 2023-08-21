2df0f26a623d713a72d2cd41fa8f68805ae5d200
Matome <seletematome3@gmail.com>
2023-08-21 09:18:13 +0200

    mobile welcome page ui done

--- movies/src/componets/Welcome/Welcome.js
+++ movies/src/componets/Welcome/Welcome.js
@@ -2,6 +2,8 @@ import React from 'react'
 
 import "./Welcome.css"
 
+// import { faCircleArrowRight } from '@fortawesome/pro-solid-svg-icons';
+
 import Welcomebg from '../../images/wecomebg.jpg';
 
 function Welcome() {
@@ -12,7 +14,7 @@ function Welcome() {
         <div className='body'>
             {/* <img className='welcomebg' src={Welcomebg} alt='wecome background'/> */}
 
-            <button className="btn btn-accent">Accent</button>
+            <button className="btn btn-accent">Next </button>   {/* <FontAwesomeIcon icon={faCircleArrowRight} /> */}
         </div>
         </div>
     </div>
